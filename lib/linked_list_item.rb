48646b5da6f0090ee99c1417f2b67301e4a0332c
Allison Higginbotham <higgintop@gmail.com>
2015-04-14 09:16:42 -0500

    Passing tests up to 04f by implementing class linked list item/node

--- lib/linked_list_item.rb
+++ lib/linked_list_item.rb
@@ -1,8 +1,46 @@
 class LinkedListItem
   include Comparable
-  attr_reader :payload
 
-  def initialize(data)
-    @payload = data
+  # automatically makes getter for payload and next_item
+  attr_reader :payload, :next_item
+
+
+  # Initialize method called for new
+  def initialize(payload = "defaultPayload", next_item = nil) 
+  	@payload = payload
+  	@next_item = next_item
   end
+
+
+  # Setter method for next_item
+  def next_item=(nextItem)
+  	 if nextItem === self
+  		raise ArgumentError
+  	 else
+  	 	@next_item = nextItem
+  	 end
+  end
+
+  # Last? method
+  def last?
+  	if @next_item == nil
+  		return true
+  	else
+  		return false
+  	end
+  end
+
+
+  # Comparable method
+  def <=>(other)
+  	return self.payload.to_s <=> other.payload.to_s
+  end
+
+  # reset the === method functionality 
+  # Comparable messes it up
+  def ===(other)
+  	return self.equal?(other)
+  end
+ 
+
 end
